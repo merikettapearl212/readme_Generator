f9fcee52f57ee51d9120179a012f8ff04b88dce6
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-12-07 19:42:36 -0800

    addded prompts

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-var inquirer = require('inquirer');
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a brief description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Describe the installation process if any:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is this project usage for?',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: '"Chose the appropriate license for this project:',
+      choices: [
+        
+    ]
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Please enter your GitHub username:',
+    },
+
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email:',
+    },
+
+  ]);
+};
