1638831b6630cee9bc41ed0b00bbb4becc2a90a5
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-12-07 20:16:59 -0800

    updated markdown

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,9 +1,36 @@
 // function to generate markdown for README
 function generateReadme(answers) {
   return `
-  
+  # ${answers.projectTitle}
+
   ## Description
   # ${answers.description}
+
+  ## Table of Contents
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Tests](#tests)
+  -[Questions](#questions)
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  ${answers.license}
+
+  ## Tests
+  ${answers.tests}
+
+  ## Contact
+  Find me on Github:
+  ${answers.username}
+  Email me:
+  ${answers.email}
 
 `;
 }
